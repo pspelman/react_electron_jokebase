d6811065099b02a443b1e5025cd3ce8c4871266f
Philip J Spelman <phil_spelman@hotmail.com>
2021-01-26 02:15:48 -0500

    - modified season-display to joke-display - more cleanup in the JokeDisplay.js - renamed SeasonDisplay.css to JokeDisplay.css

--- frontend/src/js/JokeDisplay.js
+++ frontend/src/js/JokeDisplay.js
@@ -1,44 +1,18 @@
 import React from 'react'
-import Spinner from "./components/Spinner";
-
-const getSeason = (lat, month) => {
-  console.log(`getting the season for lat ${lat} in month ${month}`, )
-  if (month > 2 && month < 9) {
-    // if we're in northern it's summer, otherwise it's in winter
-    return lat > 0 ? 'summer' : 'winter';
-  } else {  // we're in jan, feb
-    return lat > 0 ? 'winter' : 'summer'
-  }
-}
-
-const seasonConfig = {
-  winter: {
-    text: 'Brr! it\'s chilly outside',
-    iconName: 'snowflake'
-  },
-  summer: {
-    text: 'Let\'s hit the beach!',
-    iconName: 'sun'
-  }
-}
 
 const JokeDisplay = props => {
-  console.log(`the current season: `, getSeason(props.lat, new Date().getMonth()))
-  const season = props.lat ? getSeason(props.lat, new Date().getMonth()) : ''
-
-  // const seasonText = season === 'winter' ? 'Brr! it\'s chilly outside ' : 'Let\'s hit the beach!'
-  // const icon = season === 'winter' ? 'snowflake' : 'sun'
+  const joke = props.joke ? props.joke : 'no joking around... yet'
   const iconStyle = props.lat ? {display: 'block'} : {display: 'none'}
 
-  let seasonIcon = (position) => season ? <i className={`${position} massive ${seasonConfig[season].iconName} icon`} style={iconStyle} /> : ''
-
   return (
-    <div className={`season-display ${season}`}>
-      {season ? <i className={`icon-left massive ${seasonConfig[season].iconName} icon`} style={iconStyle} /> : ''}
+    <div className={`joke-display ${joke}`}>
       <br/>
-      <button className={'btn'} onClick={props.updateLocation}>Update Location</button>
+      <button className={'btn'}
+              onClick={props.updateLocation}>
+        Update Location
+      </button>
       <br/>
-      {props.lat ? seasonConfig[season].text : 'SPINNER'}
+      {props.lat ? jokeConfig[joke].text : 'SPINNER'}
       <div className="content">
         <span className="date">{new Date().toDateString()}</span>
         <br/>
@@ -46,7 +20,7 @@ const JokeDisplay = props => {
           Current Latitude: {props.lat ? `Latitude: ${props.lat}` : 'SPINNER'}
         </div>
       </div>
-      {season ? <i className={`icon-right massive ${seasonConfig[season].iconName} icon`} style={iconStyle} /> : ''}
+      {joke ? <i className={`icon-right massive ${jokeConfig[joke].iconName} icon`} style={iconStyle} /> : ''}
     </div>
   );
 }
