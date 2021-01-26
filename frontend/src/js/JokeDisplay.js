3ca0205adfebdcedd966aba6ae07a8adc45df265
Philip J Spelman <phil_spelman@hotmail.com>
2021-01-26 02:20:55 -0500

    - updated JokeDisplay.js to be a class-based component

--- frontend/src/js/JokeDisplay.js
+++ frontend/src/js/JokeDisplay.js
@@ -1,28 +1,28 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const JokeDisplay = props => {
-  const joke = props.joke ? props.joke : 'no joking around... yet'
-  const iconStyle = props.lat ? {display: 'block'} : {display: 'none'}
+class JokeDisplay extends Component {
 
-  return (
-    <div className={`joke-display ${joke}`}>
-      <br/>
-      <button className={'btn'}
-              onClick={props.updateLocation}>
-        Update Location
-      </button>
-      <br/>
-      {props.lat ? jokeConfig[joke].text : 'SPINNER'}
-      <div className="content">
-        <span className="date">{new Date().toDateString()}</span>
-        <br/>
-        <div className="description">
-          Current Latitude: {props.lat ? `Latitude: ${props.lat}` : 'SPINNER'}
-        </div>
-      </div>
-      {joke ? <i className={`icon-right massive ${jokeConfig[joke].iconName} icon`} style={iconStyle} /> : ''}
-    </div>
-  );
+	render() {
+		const joke = props.joke ? props.joke : 'no joking around... yet'
+		const iconStyle = props.lat ? {display: 'block'} : {display: 'none'}
+
+		return (
+			<div className={`joke-display ${joke}`}>
+				<br/>
+				<button className={'btn'}
+				        onClick={() => console.log(`you liked the joke`)}
+				>
+					Like this joke
+				</button>
+				<br/>
+				<div className="content">
+					<div className="description">
+						A joke description
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
 
 // Todo: add the joke date later
