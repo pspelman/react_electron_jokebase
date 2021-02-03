cfca2506bb112dbf26d7a95e38b2ec598317591a
Philip Spelman <philip.spelman@vaporstream.com>
2021-02-02 22:23:13 -0500

    - added a big button

---
+++ frontend/src/js/components/shared/BigButton.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const BigButton = () => {
+  // Note: aspects of a button
+  //    event listener so it can do something
+  //    color
+  //    size
+  //    TEXT ON THE FUCKING BUTTON
+    return (
+        <React.Fragment>
+          <div className={'big-button'}>
+            I am a big button for SAVING
+          </div>
+        </React.Fragment>
+    )
+}
+
+export default BigButton
